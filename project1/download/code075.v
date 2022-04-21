50a7963e197fe97bf7755ad8abe526487ce2a832
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-21 09:45:30 +0900

    4-bit counter

--- project1/download/code075.v
+++ project1/download/code075.v
@@ -17,16 +17,26 @@ endmodule
 
 module m_4ADDER (w_a, w_b, w_s);
  /* Please describe here by yourself */
+  input wire [3:0] w_a, w_b;
+  output wire [3:0] w_s;
+
+  wire w_cin[4:0];
+  assign w_cin[0] = 1'b0;
+  
+  m_FA m_FA0 (w_a[0], w_b[0], w_cin[0], w_s[0], w_cin[1]);
+  m_FA m_FA1 (w_a[1], w_b[1], w_cin[1], w_s[1], w_cin[2]);
+  m_FA m_FA2 (w_a[2], w_b[2], w_cin[2], w_s[2], w_cin[3]);
+  m_FA m_FA3 (w_a[3], w_b[3], w_cin[3], w_s[3], w_cin[4]);
 endmodule
 
 module m_FA (w_a, w_b, w_cin, w_s, w_cout);
-   input wire w_a, w_b, w_cin;
-   output wire w_s, w_cout;
+  input wire w_a, w_b, w_cin;
+  output wire w_s, w_cout;
 
-   wire w_e, w_f, w_g;
-   m_HA m_HA0(w_cin, w_e, w_s, w_g);
-   m_HA m_HA1(w_a, w_b, w_e, w_f);
-   assign w_cout = w_g | w_f;
+  wire w_e, w_f, w_g;
+  m_HA m_HA0(w_cin, w_e, w_s, w_g);
+  m_HA m_HA1(w_a, w_b, w_e, w_f);
+  assign w_cout = w_g | w_f;
 endmodule
 
 module m_HA (w_a, w_b, w_s, w_c);
