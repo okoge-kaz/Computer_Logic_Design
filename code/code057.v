660005c0c27d695ff483ebdf4fb688d29fa33488
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-25 11:08:44 +0900

    formatting

--- code/code057.v
+++ code/code057.v
@@ -8,8 +8,8 @@ module m_main (w_clk, w_led);
   reg [3:0]  r_out = 4'b0000;
   reg [31:0] r_cnt = 0;
   always@(posedge w_clk) begin
-    r_cnt <= (r_cnt==99999999) ? 0 : r_cnt +1;
-    r_out <= (r_cnt==0) ? r_out +1 : r_out;
+    r_cnt <= (r_cnt==99999999) ? 0 : r_cnt + 1;
+    r_out <= (r_cnt==0) ? r_out + 1 : r_out;
   end
   assign w_led = r_out;
   // vio_0 vio_00(w_clk, w_led[3], w_led[2], w_led[1], w_led[0]);
