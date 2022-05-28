389077eb68b0987f15267d5df2764a56e89cc520
Fujii Kazuki <kaz.tokyo.tech20@gmail.com>
2022-05-28 23:59:24 +0900

    initial design contest

--- design_contest/temp.v
+++ design_contest/temp.v
@@ -85,6 +85,7 @@ module m_amemory (w_clk, w_addr, w_we, w_din, w_dout);
 `include "program.txt"
 endmodule
 
+
 module m_regfile (w_clk, w_rr1, w_rr2, w_wr, w_we, w_wdata, w_rdata1, w_rdata2);
    input  wire        w_clk;
    input  wire [4:0]  w_rr1, w_rr2, w_wr;
@@ -93,13 +94,14 @@ module m_regfile (w_clk, w_rr1, w_rr2, w_wr, w_we, w_wdata, w_rdata1, w_rdata2);
    output wire [31:0] w_rdata1, w_rdata2;
     
    reg [31:0] r[0:31];
-   assign w_rdata1 = (w_rr1==0) ? 0 : r[w_rr1];
-   assign w_rdata2 = (w_rr2==0) ? 0 : r[w_rr2];
+   assign w_rdata1 = (w_rr1==0) ? 0 : (w_rr1==w_wr) ? w_wdata : r[w_rr1];
+   assign w_rdata2 = (w_rr2==0) ? 0 : (w_rr2==w_wr) ? w_wdata : r[w_rr2];
    always @(posedge w_clk) if(w_we) r[w_wr] <= w_wdata;
    
    initial r[1] = 1;
    initial r[2] = 2;
 endmodule
+
 
 module m_proc07 (w_clk, r_led);
    input  wire w_clk;
